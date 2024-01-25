b75a5ec15e77fbf0c6a467aa72df5c900e54aa6c
bokor3288 <157743109+bokor3288@users.noreply.github.com>
2024-01-26 01:55:21 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,47 @@
+ <div id="contact-page" class="container">
+        <div class="bg">      
+            <div class="row">   
+                <div class="col-sm-8">
+                    <div class="contact-form">
+                        <h2 class="title text-center">BUBT Medicine Shop</h2>
+                        <div class="status alert alert-success" style="display: none"></div>
+                        <div class="col-md-4 clearfix">
+                    <div class="logo pull-left">
+                    <a href="<?php echo web_root?>"><img src="images/home/bubt7.png" alt="" /></a>
+                    </div> 
+                    </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="contact-info">
+                        <h2 class="title text-center">Contact Info</h2>
+                        <address>
+                            <p>BUBT Medicine Shop.</p>
+                            <p>Plot # 77-78, Road # 9, Rupnagar R/A,</p>
+                            <p>Mirpur-2, Dhaka 1216, Bangladesh</p>
+                            <p>Mobile: +01700000000</p>
+                            <p>Email: bubt@gmail.com</p>
+                        </address>
+                        <div class="social-networks">
+                            <h2 class="title text-center">Social Networking</h2>
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-youtube"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>              
+            </div>  
+        </div>  
+    </div><!--/#contact-page-->
